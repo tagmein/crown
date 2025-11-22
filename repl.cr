3849cf6db5fdc71123f35da1fae711d5dbf7ce56
Nate Ferrero <nate@tagme.in>
2025-11-21 22:13:05 -0600

    partial parse

--- repl.cr
+++ repl.cr
@@ -1,16 +1,26 @@
 clone session
 global process stdin setEncoding, call utf8
 
-set prompt [ function [
- global process stdout write, call '> '
-] ]
+set prompt [
+ function prefix [
+  get prefix, true [
+   global process stdout write, call [
+    template '%0 ... > ' [ get prefix, default '' ]
+   ]
+  ], false [
+   global process stdout write, call '> '
+  ]
+ ]
+]
 get prompt, call
 
 global process stdin on, call data [
  function chunk [
   get session clear_error, call        
-  get session run, call [ get chunk trim, call ]
-  log [ get session current, call ]
-  get prompt, call
+  get session run, call [ get chunk ]
+  get session prefix, false [
+   log [ get session current, call ]
+  ]
+  get prompt, call [ get session prefix ]
  ]
 ]
