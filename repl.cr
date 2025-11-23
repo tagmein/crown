5fbebda15faaa0c89af5cfc7aa0617668928a8d7
Nate Ferrero <nate@tagme.in>
2025-11-23 11:17:14 -0600

    import, spread, error cleanup, repl

--- repl.cr
+++ repl.cr
@@ -16,11 +16,20 @@ get prompt, call
 
 global process stdin on, call data [
  function chunk [
-  get session clear_error, call        
-  get session run, call [ get chunk ]
-  get session prefix, false [
-   log [ get session current, call ]
+  get chunk trim, call, is 'clear', true [
+   global setTimeout
+   call [ function [
+     global console clear, call
+     get prompt, call [ get session prefix ]
+   ] ]
   ]
-  get prompt, call [ get session prefix ]
+  false [
+   get session clear_error, call
+   get session run, call [ get chunk ]
+   get session prefix, false [
+    log '*' [ get session current, call ]
+   ]
+   get prompt, call [ get session prefix ]
+  ]
  ]
 ]
